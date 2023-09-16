6f0d2e4e0415c6139a2fba2f075073ab7d98a25e
goolkin <samsonik2000@gmail.com>
2023-09-17 00:52:49 +0300

    Error 404 handled in Controller.java

--- src/main/java/com/test1/demo/controllers/Controller.java
+++ src/main/java/com/test1/demo/controllers/Controller.java
@@ -4,8 +4,10 @@ import com.test1.demo.Services.FrequencyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -16,8 +18,20 @@ public class Controller {
     private FrequencyService frequencyService;
 
     @PostMapping("/calculate-frequency")
-    public ResponseEntity<Map<Character, Integer>> calculateFrequency(@RequestBody String input) {
-        Map<Character, Integer> frequencyMap = frequencyService.calculateCharacterFrequency(input);
-        return new ResponseEntity<>(frequencyMap, HttpStatus.CREATED);
+    public ResponseEntity<Map<Character, Integer>> calculateFrequency(@RequestBody(required = false) String input) {
+        if (input == null || input.isEmpty()) {
+            Map<Character, Integer> errorResponse = new HashMap<>();
+            errorResponse.put('e', 1);
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            Map<Character, Integer> frequencyMap = frequencyService.calculateCharacterFrequency(input);
+            return new ResponseEntity<>(frequencyMap, HttpStatus.CREATED);
+        } catch (HttpMessageNotReadableException e) {
+            System.out.println("An error occurred: " + e.toString());
+            return new ResponseEntity<>(new HashMap<>(), HttpStatus.BAD_REQUEST);
+        }
     }
+
 }
